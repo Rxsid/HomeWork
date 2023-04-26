fd8e5439359953183593aee665b03d9a15505da1
Andrey Sidorov <rxsid@mail.ru>
2023-04-26 22:07:46 +0300

    added task6

---
+++ HW1/task6/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 6: Напишите программу, которая на вход принимает число и выдаёт,
+// является ли число чётным (делится ли оно на два без остатка).
+
+// 4 -> да
+// -3 -> нет
+// 7 -> нет
+
+Console.Write("Введите число A: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+int mod = numberA % 2;
+if (mod == 0)
+{
+Console.Write($"Число {numberA} - четное");
+}
+else
+{
+Console.WriteLine($"Число{numberA} - нечетное");
+}
